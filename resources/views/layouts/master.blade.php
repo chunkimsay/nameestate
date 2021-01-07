b74f5985d0147b02c1c0383fd2484a08bc654e64
unknown <chun.kimsa99@gmail.com>
2021-01-07 21:57:07 +0700

    chat

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -124,7 +124,15 @@
           <div class="line w-100 bg-secondary mt-3" style="height: 2px;opacity: 70%;"></div>
         </div>
      </div>
-        
+     <div id="fb-root"></div>
+        <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+      attribution=setup_tool
+      page_id="103845804909181"
+      theme_color="#000030"
+      logged_in_greeting="Hi! How can we help you? Do you interest with which real estate"
+      logged_out_greeting="Hi! How can we help you? Do you interest with which real estate">
+    </div>
         <br>
           <!-- endnavbar -->
           <!-- slide -->
@@ -179,6 +187,21 @@
      <script>
             $(".chosen-select").chosen();
   </script>
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml            : true,
+        version          : 'v9.0'
+      });
+    };
+
+    (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
         @yield('js')
 </body>
 </html>
